96839c6fd6dd83ab64522f589333301c4f8b4898
Sandy <sandychn@163.com>
2020-10-01 14:32:43 +0800

    update lmsdb.sql

--- sql/lmsdb.sql
+++ sql/lmsdb.sql
@@ -24,7 +24,7 @@ CREATE TABLE book
     bClassId INT            NOT NULL,
     bCount   INT            NOT NULL CHECK(bCount >= 0),
     bSum     INT            NOT NULL CHECK(bSum >= 0),
-    FOREIGN KEY (bClassId) REFERENCES book_class(bClassId) 
+    FOREIGN KEY (bClassId)  REFERENCES book_class(bClassId) 
 ) CHARSET=utf8;
 
 CREATE TABLE book_lending
@@ -36,10 +36,10 @@ CREATE TABLE book_lending
     brTime  DATETIME
 ) CHARSET=utf8;
 
-INSERT INTO lib_user VALUES ('USEr1', '123456', 1);
-INSERT INTO lib_user VALUES ('USEr2', '123456', 2);
-INSERT INTO lib_user VALUES ('USEr3', '123456', 3);
-INSERT INTO lib_user VALUES ('USEr4', '123456', 4);
+INSERT INTO lib_user VALUES ('user1', '123456', 1);
+INSERT INTO lib_user VALUES ('user2', '123456', 2);
+INSERT INTO lib_user VALUES ('user3', '123456', 3);
+INSERT INTO lib_user VALUES ('user4', '123456', 4);
 
 INSERT INTO book_class VALUES (1, '农业科学');
 INSERT INTO book_class VALUES (2, '哲学');
@@ -63,4 +63,4 @@ INSERT INTO book VALUES ('9789571189680', '新時代的醫學人文', '戴正德/石曜堂', '
 INSERT INTO book VALUES ('9787811168730', '医学文献检索', '谢志耘', '北京大学医学出版社', 3, 5, 5);
 INSERT INTO book VALUES ('9789863185956', '說英語Fun遊台灣', 'Andrew Crosthwaite', '寂天文化事業股份有限公司', 8, 2, 2);
 INSERT INTO book VALUES ('9787502474560', '单质硼及其应用', '倪坤', '冶金工业出版社', 11, 3, 3);
-INSERT INTO book VALUES ('9787504668974', '世界前沿技术发展报告:2014', '中国科学技术协会国际联络部', '中国科技术出版社', 8, 4, 4);
+INSERT INTO book VALUES ('9787504668974', '世界前沿技术发展报告:2014', '中国科学技术协会国际联络部', '中国科技技术出版社', 8, 4, 4);
